9a204a5f42d72110adce8d49213c4d0baa14c3f8
sagharnoursobhi <saghar.noursobhi@hyperisland.se>
2021-03-31 20:28:20 +0200

    final commit

--- src/Secured.js
+++ src/Secured.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Keycloak from 'keycloak-js';
+import UserInfo from './UserInfo';
+import Logout from './Logout';
 
 class Secured extends Component {
 
@@ -9,18 +11,20 @@ class Secured extends Component {
   }
 
   componentDidMount() {
-    const keycloak = Keycloak('/Keycloak.json');
+    const keycloak = Keycloak('/keycloak.json');
     keycloak.init({onLoad: 'login-required'}).then(authenticated => {
-      this.setState({ Keycloak: Keycloak, authenticated: authenticated })
+      this.setState({ keycloak: keycloak, authenticated: authenticated })
     })
   }
 
   render() {
-    if (this.state.Keycloak) {
-      if (this.state.authenticated) return (
+    if(this.state.keycloak) {
+      if(this.state.authenticated) return (
         <div>
           <p>This is a Keycloak-secured component of your application. You shouldn't be able
           to see this unless you've authenticated with Keycloak.</p>
+          <UserInfo keycloak={this.state.keycloak} />
+          <Logout keycloak={this.state.keycloak} />
         </div>
       ); else return (<div>Unable to authenticate!</div>)
     }
